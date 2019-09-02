9c427c7928048e2172c05993297b4b11789f578f
Al Frost <alex13-92@yandex.ru>
2019-09-02 20:05:33 +0300

    added do / while circle for endless game

--- src/com/packages/Main.java
+++ src/com/packages/Main.java
@@ -7,21 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int theNumber = (int)(Math.random()* 100 + 1);
-        System.out.println(theNumber);
-        int guess = 0;
-        while (guess != theNumber) {
-            System.out.println("Guess a number between 1 and 100.");
-            guess = scan.nextInt();
-            System.out.println("You entered " + guess + ".");
-            if (guess < theNumber){
-              System.out.println(guess + " is too low. Try again");
-            } else if (guess > theNumber){
-              System.out.println(guess + " is too high. Try again");
-            } else {
-              System.out.println(guess + " is correct. You win!");
+        String playAgain = "";
+        do {
+            int theNumber = (int) (Math.random() * 100 + 1);
+            System.out.println(theNumber);
+            int guess = 0;
+            while (guess != theNumber) {
+                System.out.println("Guess a number between 1 and 100.");
+                guess = scan.nextInt();
+                System.out.println("You entered " + guess + ".");
+                if (guess < theNumber) {
+                    System.out.println(guess + " is too low. Try again");
+                } else if (guess > theNumber) {
+                    System.out.println(guess + " is too high. Try again");
+                } else {
+                    System.out.println(guess + " is correct. You win!");
+                }
             }
-        }
-    }
+            System.out.println("Would you like to play again (y / n)?");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y"));
 
-}
+        }
+
+        }
