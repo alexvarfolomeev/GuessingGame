3c5f11ae5ba663902c377920605f852f484969d9
Al Frost <alex13-92@yandex.ru>
2019-09-02 19:55:59 +0300

    initial commit

---
+++ src/com/packages/Main.java
@@ -0,0 +1,27 @@
+package com.packages;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int theNumber = (int)(Math.random()* 100 + 1);
+        System.out.println(theNumber);
+        int guess = 0;
+        while (guess != theNumber) {
+            System.out.println("Guess a number between 1 and 100.");
+            guess = scan.nextInt();
+            System.out.println("You entered " + guess + ".");
+            if (guess < theNumber){
+              System.out.println(guess + " is too low. Try again");
+            } else if (guess > theNumber){
+              System.out.println(guess + " is too high. Try again");
+            } else {
+              System.out.println(guess + " is correct. You win!");
+            }
+        }
+    }
+
+}
