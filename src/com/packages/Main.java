259d4227f6b32d1721bb48ec0c3e7a99c5670b6f
Al Frost <alex13-92@yandex.ru>
2019-09-03 13:14:42 +0300

    changed counter condition

--- src/com/packages/Main.java
+++ src/com/packages/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String playAgain = "";
-        int counter = 0;
         do {
             int theNumber = (int) (Math.random() * 100 + 1);
             System.out.println(theNumber);
             int guess = 0;
+            int counter = 0;
             while (guess != theNumber) {
                 System.out.println("Guess a number between 1 and 100.");
                 guess = scan.nextInt();
@@ -23,14 +23,13 @@ public class Main {
                     System.out.println(guess + " is too high. Try again");
                 } else {
                     System.out.println(guess + " is correct. You win!");
-                }
-                counter = counter + 1;
-                System.out.println("You made " + counter + " attempts. Nice try!");
+                } counter = counter + 1;
             }
+            System.out.println("You made " + counter + " attempt(s). Nice try!");
             System.out.println("Would you like to play again (y / n)?");
             playAgain = scan.next();
-        } while (playAgain.equalsIgnoreCase("y"));
 
+        } while (playAgain.equalsIgnoreCase("y"));
           System.out.println("Thank you for playing!Goodbye!");
           scan.close();
         }
